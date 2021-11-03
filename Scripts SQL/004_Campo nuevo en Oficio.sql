440e7466614b3aeddb0df9c1a252259d767830ec
Ernesto Carnot <ernesto.carnot@gmail.com>
2021-11-03 18:51:14 -0300

    Nuevo script 004 campo nuevo en oficio

---
+++ Scripts SQL/004_Campo nuevo en Oficio.sql
@@ -0,0 +1,20 @@
+﻿ALTER TABLE dbo.Oficio
+ADD UltimoUsuarioFlujoId int NULL
+GO
+
+ALTER TABLE dbo.Oficio
+ADD CONSTRAINT FK_Oficio_UltimoUsuarioFlujoId FOREIGN KEY (UltimoUsuarioFlujoId) 
+  REFERENCES dbo.Usuario (Id) 
+  ON UPDATE NO ACTION
+  ON DELETE NO ACTION
+GO
+
+UPDATE ofi
+  	SET UnidadTecnicaId = re.UtAsignadaId
+  FROM Oficio ofi
+  JOIN RequerimientosOficios ro ON ofi.Id = ro.OficioId
+  JOIN Requerimiento re ON re.Id = ro.RequerimientoId  
+ 
+UPDATE Oficio
+	SET UltimoUsuarioFlujoId = UsuarioModificacionId
+GO

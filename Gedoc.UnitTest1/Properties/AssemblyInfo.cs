bd01897faba904d1358316cb1fe71992cbeaa748
camilorsreyes <camilo.soto@testgroup.cl>
2021-11-19 18:27:20 -0300

    Pruebas Unitarias

---
+++ Gedoc.UnitTest1/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Gedoc.UnitTest1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("Gedoc.UnitTest1")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("3cfb0874-4116-417e-b508-4313e079c0b2")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
